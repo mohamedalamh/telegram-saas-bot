3e19265b0374387fdddbe14e72b7dd136c1d6f32
mohamedalamh <ssef16129@gmail.com>
2026-06-09 04:37:47 +0300

    Update web.py

--- web.py
+++ web.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
-import sqlite3
 
 app = FastAPI()
 
-conn = sqlite3.connect("users.db", check_same_thread=False)
-cursor = conn.cursor()
+@app.get("/")
+def home():
+    return {"status": "running"}
 
 @app.get("/users")
 def users():
-    cursor.execute("SELECT * FROM users")
-    return cursor.fetchall()
-
-@app.get("/status/{user_id}")
-def status(user_id: int):
-    cursor.execute("SELECT status FROM users WHERE user_id=?", (user_id,))
-    return {"status": cursor.fetchone()}
+    return {"message": "dashboard working"}
