3b3597c2216886b9d4ce366e15d552f007d5d3cc
mohamedalamh <ssef16129@gmail.com>
2026-06-09 06:39:39 +0300

    Update BOT_TOKEN and ADMIN_ID in config.py
    Replaced environment variable values for BOT_TOKEN and ADMIN_ID with hardcoded values.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+BOT_TOKEN = os.getenv("8889503880:AAEh6BR2kMi2FaHJz12eCak62LPL9m9bjMg")
+ADMIN_ID = int(os.getenv("834033986", "0"))
