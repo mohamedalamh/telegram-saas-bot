889d6a2882c08048862571e1e717c4591c0db9f5
mohamedalamh <ssef16129@gmail.com>
2026-06-09 08:47:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from keyboards import main_menu
+import database as db
+async def start(update, context):
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+
+    db.add_user(user.id, user.username)
 
     await update.message.reply_text(
-        "أهلاً بك في النظام",
+        "أهلاً بك 👋\nالنظام جاهز للعمل",
         reply_markup=main_menu()
     )
 
@@ -15,9 +19,8 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.answer()
 
     if query.data == "add_token":
-        await query.message.reply_text(
-            "أرسل توكن البوت"
-        )
+    await query.message.reply_text("أرسل توكن البوت الآن")
+    context.user_data["wait_token"] = True
 
     elif query.data == "start_bot":
         await query.message.reply_text(
@@ -38,10 +41,23 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await query.message.reply_text(
             "معلومات الاشتراك"
         )
-async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def text_handler(update, context):
 
+    user_id = update.effective_user.id
     text = update.message.text
 
+    # استقبال التوكن
+    if context.user_data.get("wait_token"):
+
+        db.save_bot(user_id, text)
+
+        context.user_data["wait_token"] = False
+
+        await update.message.reply_text(
+            "✅ تم حفظ التوكن بنجاح"
+        )
+        return
+
     await update.message.reply_text(
-        f"استلمت:\n{text}"
+        "استخدم الأزرار للتحكم 👇"
     )
