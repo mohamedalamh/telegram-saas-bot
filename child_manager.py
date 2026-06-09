3ce01c1485b2786aa4d60106d54f52ccc8bbbae1
mohamedalamh <ssef16129@gmail.com>
2026-06-10 02:19:44 +0300

    Implement start and stop functions for bots

--- child_manager.py
+++ child_manager.py
@@ -1,1 +1,21 @@
+running_bots = {}
 
+
+def start_bot(user_id, token):
+
+    if not token:
+        return False
+
+    running_bots[user_id] = token
+
+    return True
+
+
+def stop_bot(user_id):
+
+    if user_id in running_bots:
+        del running_bots[user_id]
+
+        return True
+
+    return False
