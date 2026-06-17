08ce083ff4721af9fd60c4d90ba735f826c16a33
mohamedalamh <ssef16129@gmail.com>
2026-06-17 04:34:41 +0300

    Add TelegramClientManager class with async methods

--- telegram_checker/telegram_client.py
+++ telegram_checker/telegram_client.py
@@ -1,1 +1,13 @@
+class TelegramClientManager:
 
+    async def connect()
+
+    async def disconnect()
+
+    async def check_number()
+
+    async def send_code()
+
+    async def login()
+
+    async def logout()
