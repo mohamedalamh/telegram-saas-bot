2d4371efcce2c6f1cd5fd3603dfe8497b1bc3241
mohamedalamh <ssef16129@gmail.com>
2026-06-17 04:44:37 +0300

    Remove method stubs in TelegramClientManager
    Removed method definitions from TelegramClientManager.

--- telegram_checker/telegram_client.py
+++ telegram_checker/telegram_client.py
@@ -1,13 +1,2 @@
 class TelegramClientManager:
-
-    async def connect()
-
-    async def disconnect()
-
-    async def check_number()
-
-    async def send_code()
-
-    async def login()
-
-    async def logout()
+    pass
