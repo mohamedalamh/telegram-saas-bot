7882babe107b08db2303e9c3f39f8a2e138bee10
mohamedalamh <ssef16129@gmail.com>
2026-06-17 04:33:52 +0300

    Add AccountManager class with async methods

--- telegram_checker/account_manager.py
+++ telegram_checker/account_manager.py
@@ -1,1 +1,15 @@
+class AccountManager:
 
+    async def get_best_account()
+
+    async def mark_flood()
+
+    async def increase_checks()
+
+    async def disable_account()
+
+    async def get_all_accounts()
+
+    async def get_account()
+
+    async def refresh_accounts()
