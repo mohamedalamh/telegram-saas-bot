8e4f49bdbfebf50ef0aacca3018a5507104feeba
mohamedalamh <ssef16129@gmail.com>
2026-06-17 06:26:41 +0300

    Update flood_manager.py

--- telegram_checker/flood_manager.py
+++ telegram_checker/flood_manager.py
@@ -1,7 +1,38 @@
+from datetime import datetime, timedelta
+
+from database import (
+    set_account_flood,
+    increase_account_checks
+)
+
+
 class FloodManager:
 
-    async def enter_flood()
+    async def set_flood(self, account_id, seconds):
+        """
+        عند دخول الحساب FloodWait.
+        """
 
-    async def leave_flood()
+        flood_until = datetime.utcnow() + timedelta(seconds=seconds)
 
-    async def can_use()
+        set_account_flood(
+            account_id,
+            flood_until
+        )
+
+    async def account_used(self, account_id):
+        """
+        زيادة عداد الفحص.
+        """
+
+        increase_account_checks(account_id)
+
+    async def account_ok(self, account_id):
+        """
+        حالياً لا نحتاج أي شيء هنا.
+        """
+
+        return True
+
+
+flood_manager = FloodManager()
